c081e2704bed6c3602f8864892d79805987ee9f5
tuphan-dn <tuphan@descartes.network>
2023-10-06 10:55:50 +0700

    chore: remove contents

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,8 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
-import 'hardhat-abi-exporter'
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
-  abiExporter: {
-    path: './abi',
-    runOnCompile: true,
-    clear: true,
-    flat: true,
-    only: [':MerkleDistribution$'],
-  },
 }
 
 export default config
--- src/tree.ts
+++ src/tree.ts
@@ -21,17 +21,8 @@ export class Tree {
   /**
    * The merkle root.
    */
-  get root() {
-    let nodes = this.leaves.map((leaves) => new Node(leaves.value))
-    while (nodes.length > 1) {
-      const cache: Node[] = []
-      for (let i = 0; i < nodes.length; i += 2) {
-        if (i + 1 < nodes.length) cache.push(nodes[i].hash(nodes[i + 1]))
-        else cache.push(nodes[i])
-      }
-      nodes = cache
-    }
-    return nodes[0]
+  get root(): Node {
+    // TODO
   }
 
   /**
@@ -39,31 +30,8 @@ export class Tree {
    * @param leaf Leaf.
    * @returns Proof - The list of nodes.
    */
-  prove(leaf: Leaf) {
-    let proof: Node[] = []
-    let node = new Node(leaf.value)
-    let siblings = this.leaves.map((leaf) => new Node(leaf.value))
-    while (!node.eq(this.root)) {
-      // Find my sibling
-      const index = siblings.findIndex((sibling) => node.eq(sibling))
-      if (index === -1) throw new Error('The leaf is not valid.')
-      let sibling: Node | undefined = undefined
-      if (index % 2 === 1) sibling = siblings[index - 1]
-      else if (index + 1 < siblings.length) sibling = siblings[index + 1]
-      if (sibling) {
-        node = node.hash(sibling)
-        proof.push(sibling)
-      }
-      // Move to upper level
-      const cache: Node[] = []
-      for (let i = 0; i < siblings.length; i += 2) {
-        if (i + 1 < siblings.length)
-          cache.push(siblings[i].hash(siblings[i + 1]))
-        else cache.push(siblings[i])
-      }
-      siblings = cache
-    }
-    return proof
+  prove(leaf: Leaf): Node[] {
+    // TODO
   }
 
   /**
@@ -72,9 +40,7 @@ export class Tree {
    * @param proof The proof to the leaf.
    * @returns true/false
    */
-  verify(leaf: Leaf, proof: Node[]) {
-    let node = new Node(leaf.value)
-    for (let i = 0; i < proof.length; i++) node = node.hash(proof[i])
-    return this.root.eq(node)
+  verify(leaf: Leaf, proof: Node[]): boolean {
+    // TODO
   }
 }
